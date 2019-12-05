90708ec45ab5b163b3bc7a2a2c86184410d1310c
Emilien REGENT <e.regent@owlient.eu>
2019-12-05 19:46:44 +0100

    First draft of customization
    Change-Id: I6d7ed63b1afe06aaafa31ceeebfc727498476b7c

---
+++ Assets/Sources/CustomizationPartController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class CustomizationPartController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _parts = null;
+
+    private uint _currentPartIndex = 0;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0) == true && GetClick() == true)
+        {
+            _parts[_currentPartIndex].SetActive(false);
+
+            if (++ _currentPartIndex >= _parts.Length)
+            {
+                _currentPartIndex = 0;
+            }
+
+            _parts[_currentPartIndex].SetActive(true);
+        }
+    }
+
+    private bool GetClick()
+    {
+        // Did we hit the surface?
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            //  Do whatever you want to detect what's been hit from the data stored in the "hit" variable - this should rotate it...
+
+            return gameObject == hit.collider.gameObject;
+
+        }
+
+        return false;
+    }
+}
