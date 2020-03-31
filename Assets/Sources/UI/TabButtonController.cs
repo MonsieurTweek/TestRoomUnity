05aede63be6d9279f17f68f2e8c45df1f7e1763f
emilienregent <emilien.regent@gmail.com>
2020-03-31 15:14:58 +0200

    Add tab system

---
+++ Assets/Sources/UI/TabButtonController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class TabButtonController : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    public TabGroupController tabGroup = null;
+
+    public Image background = null;
+
+    public UnityEvent onTabSelected;
+    public UnityEvent onTabDeselected;
+
+    private void Start()
+    {
+        background = GetComponent<Image>();
+        tabGroup.Subscribe(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.OnTabExit(this);
+    }
+
+    public void Select()
+    {
+        if (onTabSelected != null)
+        {
+            onTabSelected.Invoke();
+        }
+    }
+
+    public void Deselect()
+    {
+        if (onTabDeselected != null)
+        {
+            onTabDeselected.Invoke();
+        }
+    }
+}
---
+++ Assets/Sources/UI/TabGroupController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TabGroupController : MonoBehaviour
+{
+    public List<TabButtonController> tabButtons = null;
+    public List<GameObject> pages = null;
+
+    public Sprite tabIdle = null;
+    public Sprite tabHover = null;
+    public Sprite tabActive = null;
+
+    public TabButtonController selectedTab = null;
+
+    private void Awake()
+    {
+        if (selectedTab != null)
+        {
+            OnTabSelected(selectedTab);
+        }
+    }
+
+    public void Subscribe(TabButtonController button)
+    {
+        if (tabButtons == null)
+        {
+            tabButtons = new List<TabButtonController>();
+        }
+
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabButtonController button)
+    {
+        ResetTabs();
+
+        if (selectedTab == null || button != selectedTab)
+        {
+            button.background.sprite = tabHover;
+        }
+    }
+
+    public void OnTabExit(TabButtonController button)
+    {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(TabButtonController button)
+    {
+        if (selectedTab != null)
+        {
+            selectedTab.Deselect();
+        }
+
+        selectedTab = button;
+        selectedTab.Select();
+
+        ResetTabs();
+
+        button.background.sprite = tabActive;
+    }
+
+    public void ResetTabs()
+    {
+        foreach(TabButtonController button in tabButtons)
+        {
+            if (selectedTab == null || button != selectedTab)
+            {
+                button.background.sprite = tabIdle;
+            }
+        }
+    }
+
+    public void SwapContent()
+    {
+        int index = selectedTab.transform.GetSiblingIndex();
+
+        for (int i = 0; i < pages.Count; i++)
+        {
+            if (i == index)
+            {
+                pages[i].SetActive(true);
+            }
+            else
+            {
+                pages[i].SetActive(false);
+            }
+        }
+    }
+}
