20389c930a33bf07c57f7d3dfe117fd6575c82b9
emilienregent <emilien.regent@gmail.com>
2020-05-14 19:50:57 +0200

    Add character selection

---
+++ Assets/Sources/CharacterSelectionCanvasController.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelectionCanvasController : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _text = null;
+
+    [SerializeField]
+    private Image _image = null;
+
+    public void UpdateContent(string name, Sprite icon)
+    {
+        _text.text = name;
+        _image.sprite = icon;
+    }
+}
