50bfd551ea7032aa4b6b6a4e7d7b91a6101bd1fe
emilienregent <emilien.regent@gmail.com>
2020-05-23 19:17:33 +0200

    Integrate player locomotion and attack (sorcerer)

---
+++ Assets/Sources/PlayerOrientationController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerOrientationController : MonoBehaviour
+{
+    private Vector2 _playerRotation = Vector2.zero;
+    public float speed = 3f;
+
+    private void Awake()
+    {
+        // Save current player rotation to start with
+        _playerRotation = transform.eulerAngles;
+    }
+
+    private void Update()
+    {
+        // On unlock camera
+        if (Input.GetMouseButton(1) == true)
+        {
+            // Compute rotation based on mouse horizontal position
+            _playerRotation.y += Input.GetAxis("Mouse X");
+
+            // Apply it to player rotation
+            transform.eulerAngles = _playerRotation * speed;
+        }
+    }
+}
