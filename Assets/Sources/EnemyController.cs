4bd0e85d56fec549c7a58101c99b8ab7986456a2
Gaëtan Régent <gaetan.regent@gmail.com>
2020-06-04 11:49:12 +0200

    Prefab enemy + enemy controller

---
+++ Assets/Sources/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public GameObject TargetIndicator = null;
+
+    private void Awake()
+    {
+        SetActiveTargetIndicator(false);
+    }
+
+    public void SetActiveTargetIndicator(bool value)
+    {
+        TargetIndicator.gameObject.SetActive(value);
+    }
+}
