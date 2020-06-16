249123c09f2c62d1df029507bb907fe5b136ec91
emilienregent <emilien.regent@gmail.com>
2020-06-16 10:09:23 +0200

    Integrate Open_Sans font

---
+++ Assets/Sources/Menu/Store/StoreOfferController.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoreOfferController : MonoBehaviour
+{
+    [Header("References")]
+    public TextMeshProUGUI priceText = null;
+    public TextMeshProUGUI titleText = null;
+    public TextMeshProUGUI descriptionText = null;
+    public Image icon = null;
+
+    [Header("Properties")]
+    public StoreOffer offer = null;
+
+    private void Awake()
+    {
+        if (offer != null)
+        {
+            priceText.text = offer.price.ToString();
+            titleText.text = offer.title;
+            descriptionText.text = offer.description;
+
+            icon.sprite = offer.icon;
+        }
+    }
+}
---
+++ Assets/Sources/ScriptableObject/StoreOffer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Offer", menuName = "ScriptableObjects/Store Offer", order = 3)]
+[Serializable]
+public class StoreOffer : DescriptiveObject
+{
+    public int price = 1000;
+}
