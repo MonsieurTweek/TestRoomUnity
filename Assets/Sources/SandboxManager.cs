8c55a1322825e9b98b5b51f940d9509d6630de32
emilienregent <emilien.regent@gmail.com>
2020-06-18 10:21:08 +0200

    Fix sandbox to work as its own

---
+++ Assets/Sources/SandboxManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SandboxManager : MonoBehaviour
+{
+    public PlayerFSM player = null;
+    public EnemyFSM[] enemies = null;
+
+    private void Start()
+    {
+        if (player != null)
+        {
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i].Initialize(player);
+            }
+        }
+    }
+}
