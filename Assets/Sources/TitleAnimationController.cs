3911c30e678873df46c9e5a308a1d36642ffde76
emilienregent <emilien.regent@gmail.com>
2020-09-15 20:30:18 +0200

    Animate title intro

---
+++ Assets/Sources/TitleAnimationController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleAnimationController : MonoBehaviour
+{
+    public GameObject cameraCanvas = null;
+    public GameObject overlayCanvas = null;
+
+    public LeanTweenType introAnimation = LeanTweenType.linear;
+    public AudioClip introSound = null;
+    public AudioClip introMusic = null;
+    public float introDuration = 3f;
+    public float introDelay = 0.5f;
+
+    private void Start()
+    {
+        StartCoroutine(StartAnimation());
+    }
+
+    private IEnumerator StartAnimation()
+    {
+        yield return new WaitForSeconds(introDelay);
+
+        LeanTween.scale(cameraCanvas, Vector3.one, introDuration).setEase(introAnimation).setOnComplete(OnAnimationEnded);
+
+        AudioManager.instance.PlayMenuSound(introSound);
+        AudioManager.instance.PlayMusic(introMusic);
+    }
+
+    private void OnAnimationEnded()
+    {
+        overlayCanvas.SetActive(true);
+
+    }
+}
